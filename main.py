3a5dad97ff13ba0b40d0144901d8425bc3a72e00
Trevon Henderson <103610962+trevonhndrsn@users.noreply.github.com>
2022-11-22 12:45:15 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from tkinter import *
+import pandas
 
 BACKGROUND_COLOR = "#B1DDC6"
+
+pandas.read_csv("data/french_words.csv")
+
+def next_card():
+    
 
 
 #--------------------UI Setup------------------#
@@ -14,7 +20,16 @@ canvas.create_image(400, 263, image=card_front_img)
 canvas.create_text(400, 150, text="title", font=("Ariel", 40, "italic"))
 canvas.create_text(400, 263, text="word", font=("Ariel", 60, "bold"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
-canvas.grid(row=0, column=0)
+canvas.grid(row=0, column=0, columnspan=2)
+
+cross_image = PhotoImage(file="images/wrong.png")
+unknown_button = Button(cross_image, highlightthickness=0, command=next_card) 
+unknown_button.grid(row=1, column=0)
+
+check_image = PhotoImage(file="images/right.png")
+known_button = Button(check_image, highlightthickness=0, command=next_card)
+known_button.grid(row=1, column=1)
+
 
 window.mainloop()
 

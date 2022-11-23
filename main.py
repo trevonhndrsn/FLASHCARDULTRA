2c3a8f6ff33dea480a9815b67668cc5338814785
Trevon Henderson <103610962+trevonhndrsn@users.noreply.github.com>
2022-11-22 23:20:54 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import pandas
 
 BACKGROUND_COLOR = "#B1DDC6"
 
-pandas.read_csv("data/french_words.csv")
+data = pandas.read_csv("data/french_words.csv")
 
 def next_card():
+    pass
     
 
 
@@ -14,10 +15,10 @@ window = Tk()
 window.title("Flash Card Ultra")
 window.config(padx=50, pady=50, bg = BACKGROUND_COLOR)
 
-canvas = Canvas(width=800, height=526)
+canvas = Canvas(width=400, height=263)
 card_front_img = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image=card_front_img)
-canvas.create_text(400, 150, text="title", font=("Ariel", 40, "italic"))
+canvas.create_image(200, 75, image=card_front_img)
+canvas.create_text(200, 150, text="title", font=("Ariel", 40, "italic"))
 canvas.create_text(400, 263, text="word", font=("Ariel", 60, "bold"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(row=0, column=0, columnspan=2)
